a8c8a0b529c9b066053666b58f7332d458a073d7
Antonov Tatiyana <48628382+TatyanaAntonov@users.noreply.github.com>
2023-03-07 21:23:20 -0500

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -114,7 +114,7 @@
 
 	function onScroll(event){
 	    var scrollPos = $(document).scrollTop();
-	    $('.nav a[href^="#"]').each(function () {
+	    $('.nav a[href="#"]').each(function () {
 	        var currLink = $(this);
 	        var refElement = $(currLink.attr("href"));
 	        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
