fa215d37f5ed288966db6bafbaecb3ae36a298c7
Tatiyana Antonov <tintonika@gmail.com>
2023-03-22 19:48:21 -0400

    commit22-3-23

--- assets/form.php
+++ assets/form.php
@@ -1,19 +1,19 @@
 <?php
-/* Здесь проверяется существование переменных */
+
 if (isset($_POST['name'])) {$name = $_POST['name'];}
 if (isset($_POST['company'])) {$company = $_POST['company'];}
 if (isset($_POST['phone'])) {$phone = $_POST['phone'];}
 if (isset($_POST['email'])) {$email = $_POST['email'];}
 if (isset($_POST['message'])) {$message = $_POST['message'];}
   
-/* Сюда впишите свою эл. почту */
-$myaddres  = "support@asicx.com"; 
+
+$myaddres  = "Contact@asicxchange.com"; 
  
-/* А здесь прописывается текст сообщения, \n - перенос строки */
+
 $mes = "You have received a new message from the user $name ($company).\n".
 "Here is the message:\n $message".
  
-/* А эта функция как раз занимается отправкой письма на указанный вами email */
+
 $sub='New Form submission'; 
 $email="From: $name ($company)"; 
 $send = mail ($myaddres,$sub,$mes,"Content-type:text/plain; charset = utf-8\r\nFrom:$email");
@@ -29,8 +29,8 @@ header('Refresh: 3; URL=index.html');
 <title>Thank you! We will contact you!</title>
 <meta name="generator">
 <script type="text/javascript">
-setTimeout('location.replace("/index.html")', 3000);
-/*Изменить текущий адрес страницы через 3 секунды (3000 миллисекунд)*/
+setTimeout('location.replace("/index.html")', 2000);
+
 </script> 
 </head>
 <body>
