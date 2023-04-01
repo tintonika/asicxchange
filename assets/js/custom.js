5c32e2a49885cb0c3c797bcd3a7d13f728314aba
Tatiyana Antonov <tintonika@gmail.com>
2023-03-31 21:02:34 -0400

    youtube video added

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -197,3 +197,30 @@ $(document).ready(function () {
 		$(this).addClass("hide");
 	});
 });
+function setVideoCenter() {
+	var $box = $('.main-banner');
+	var height = $box.height();
+	var width = $box.width();
+	var new_height = width / 1.78;
+	if (new_height > height) {
+	  $box.find('iframe').css({
+		width: width, 
+		height: new_height, 
+		top: '-' + (new_height / 2 - height / 2) + 'px',
+		left: '0',
+	  });	
+	} else {
+	  var new_width = height * 1.78;
+	  $box.find('iframe').css({
+		width: new_width, 
+		height: height, 
+		top: '0',
+		left: '-' + (new_width / 2 - width / 2) + 'px'
+	  });
+	}	
+  }
+  
+  $(function(){
+	setVideoCenter();
+	$(window).resize(setVideoCenter);
+  });	
